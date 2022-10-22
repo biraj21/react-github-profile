e3c9d9fa0ac0f819a8b16690d621f6aa29c8c5ba
Biraj <biraj.pub@gmail.com>
2022-10-22 15:37:17 +0530

    added home button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import "./Navbar.scss";
 import { ReactComponent as GitHubSvg } from "../assets/github.svg";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
@@ -7,6 +8,9 @@ export default function Navbar() {
       <div className="wrapper">
         <GitHubSvg />
         <h1>GitHub API</h1>
+        <NavLink to="/" className={({ isActive }) => (isActive ? "active" : null)} end>
+          Home
+        </NavLink>
       </div>
     </nav>
   );
